e6ddb57e385a3a77e7a870af7773d330e1340d7e
hisham rehab <128677920+hishamrehab@users.noreply.github.com>
2023-10-01 00:48:55 +0300

    Add files via upload

---
+++ images.js
@@ -0,0 +1,8 @@
+import img1 from "./images/tour-1.jpeg";
+import img2 from "./images/tour-2.jpeg";
+import img3 from "./images/tour-3.jpeg";
+import img4 from "./images/tour-4.jpeg";
+import img5 from "./images/tour-5.jpeg";
+import img6 from "./images/tour-6.jpeg";
+
+export default [img1, img2, img3, img4, img5];
